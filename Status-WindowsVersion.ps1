eefebdb0eaa4ece706f9e5973e8c349482a1ac1a
cwlxx9 <cr29fjccyr@privaterelay.appleid.com>
2025-10-16 13:36:16 +0800

    Create Status-WindowsVersion.ps1

---
+++ Status-WindowsVersion.ps1
@@ -0,0 +1,40 @@
+# ------------------------------------------------------------
+# 💻 Section : Detect Windows Edition & Version - Start
+# ------------------------------------------------------------
+function Get-WindowsEdition {
+    $os = Get-CimInstance Win32_OperatingSystem
+    $caption = $os.Caption
+    $build = [System.Environment]::OSVersion.Version.Build
+
+    # --- Edition Detection ---
+    if ($caption -match "Home") { $edition = "Home" }
+    elseif ($caption -match "Pro") { $edition = "Pro" }
+    elseif ($caption -match "Enterprise") { $edition = "Ent" }
+    elseif ($caption -match "Education") { $edition = "Edu" }
+    else { $edition = "Edition" }
+
+    # --- Version Tag Based on Build ---
+    switch ($build) {
+        {$_ -ge 26200} { $version = "25H2"; break }
+        {$_ -ge 26100} { $version = "24H2"; break }
+        {$_ -ge 22621} { $version = "22H2"; break }
+        {$_ -ge 22000} { $version = "21H2"; break }
+        {$_ -ge 19045} { $version = "22H2"; break }
+        {$_ -ge 19044} { $version = "21H2"; break }
+        {$_ -ge 19043} { $version = "21H1"; break }
+        {$_ -ge 19042} { $version = "20H2"; break }
+        default { $version = "N/A" }
+    }
+
+    # --- Windows Generation ---
+    if ($build -ge 22000) {
+        $osVer = "Win11 $edition $version"
+    } else {
+        $osVer = "Win10 $edition $version"
+    }
+
+    return $osVer
+}
+# ------------------------------------------------------------
+# 💻 Section : Detect Windows Edition & Version - End
+# ------------------------------------------------------------
