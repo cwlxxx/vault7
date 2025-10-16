47e0a65846cbc4fd4818740a30339fcea0f13edb
cwlxx9 <cr29fjccyr@privaterelay.appleid.com>
2025-10-16 13:37:31 +0800

    Update Menu-NoGUI.ps1

--- Menu-NoGUI.ps1
+++ Menu-NoGUI.ps1
@@ -38,39 +38,17 @@ function Set-Console {
 # ------------------------------------------------------------
 # 💻 Section : Detect Windows Edition & Version - Start
 # ------------------------------------------------------------
-function Get-WindowsEdition {
-    $os = Get-CimInstance Win32_OperatingSystem
-    $caption = $os.Caption
-    $build = [System.Environment]::OSVersion.Version.Build
+try {
+    # Load the latest version from GitHub
+    $remoteScript = "https://raw.githubusercontent.com/cwlxx9/vault7/main/Status-WindowsVersion.ps1"
+    $null = irm $remoteScript | iex
 
-    # --- Edition Detection ---
-    if ($caption -match "Home") { $edition = "Home" }
-    elseif ($caption -match "Pro") { $edition = "Pro" }
-    elseif ($caption -match "Enterprise") { $edition = "Ent" }
-    elseif ($caption -match "Education") { $edition = "Edu" }
-    else { $edition = "Edition" }
-
-    # --- Version Tag Based on Build ---
-    switch ($build) {
-        {$_ -ge 26200} { $version = "25H2"; break }
-        {$_ -ge 26100} { $version = "24H2"; break }
-        {$_ -ge 22621} { $version = "22H2"; break }
-        {$_ -ge 22000} { $version = "21H2"; break }
-        {$_ -ge 19045} { $version = "22H2"; break }
-        {$_ -ge 19044} { $version = "21H2"; break }
-        {$_ -ge 19043} { $version = "21H1"; break }
-        {$_ -ge 19042} { $version = "20H2"; break }
-        default { $version = "N/A" }
-    }
-
-    # --- Windows Generation ---
-    if ($build -ge 22000) {
-        $osVer = "Win11 $edition $version"
-    } else {
-        $osVer = "Win10 $edition $version"
-    }
-
-    return $osVer
+    # Execute the function (assumed defined in Status-PowerStatus.ps1)
+    $sleepstatus = Get-PowerStatus
+}
+catch {
+    Write-Host "Error loading or executing Status-WindowsVersion.ps1: $($_.Exception.Message)" -ForegroundColor Red
+    $sleepstatus = "Error"
 }
 # ------------------------------------------------------------
 # 💻 Section : Detect Windows Edition & Version - End
@@ -263,6 +241,7 @@ Start-LiangMenu
 # ------------------------------------------------------------
 # 🚀 Section : Script Start - End
 # ------------------------------------------------------------
+
 
 
 
