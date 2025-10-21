8eb7dade837e6d2ebfa8c597c18429a1f213ff28
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-10-21 21:25:30 +0800

    Update HomePage.ps1

--- AdvanceSetupTools-PSVersion/Pages/HomePage.ps1
+++ AdvanceSetupTools-PSVersion/Pages/HomePage.ps1
@@ -1,1 +1,19 @@
+# HomePage.ps1
+Add-Type -AssemblyName PresentationFramework
+
+# Create a black Border with centered text
+$page = New-Object System.Windows.Controls.Border
+$page.Background = [Windows.Media.Brushes]::Black
+$page.Margin = '20'
+$page.CornerRadius = '8'
+
+$text = New-Object System.Windows.Controls.TextBlock
+$text.Text = "This is HomePage.ps1 file"
+$text.Foreground = [Windows.Media.Brushes]::White
+$text.FontSize = 22
+$text.HorizontalAlignment = 'Center'
+$text.VerticalAlignment = 'Center'
+
+$page.Child = $text
+return $page
 
