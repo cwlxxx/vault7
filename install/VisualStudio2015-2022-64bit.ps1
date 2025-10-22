e0c81a6eff7caca9c6e4f2cda1c8b59cbb924385
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-10-22 19:29:12 +0800

    Update VisualStudio2015-2022-64bit.ps1

--- install/VisualStudio2015-2022-64bit.ps1
+++ install/VisualStudio2015-2022-64bit.ps1
@@ -4,7 +4,7 @@ Write-Host "🔍 Checking for Visual C++ 2015–2022 (x64)..."
 
 if (-not (winget list --id Microsoft.VCRedist.2015+.x64 | Select-String "Installed")) {
     Write-Host "📦 Installing Visual C++ 2015–2022 (x64)..." -ForegroundColor Cyan
-    winget install --id Microsoft.VCRedist.2015+.x64 --source winget --accept-package-agreements --accept-source-agreements
+    winget install --id Microsoft.VCRedist.2015+.x64 --source winget --exact --accept-package-agreements --accept-source-agreements
     Write-Host "✅ Installation completed." -ForegroundColor Green
 } else {
     Write-Host "✅ Visual C++ 2015–2022 (x64) is already installed. Skipping." -ForegroundColor Yellow
