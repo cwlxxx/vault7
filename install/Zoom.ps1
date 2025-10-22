7307d87b9416df1eb08958ddfd89f64c1db9ed59
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-10-22 19:28:40 +0800

    Update Zoom.ps1

--- install/Zoom.ps1
+++ install/Zoom.ps1
@@ -5,7 +5,7 @@
 Write-Host "Installing Zoom..." -ForegroundColor Cyan
 
 # Run the Winget installation (silent)
-Start-Process "winget.exe" -ArgumentList "install --id Zoom.Zoom --source winget --accept-package-agreements --accept-source-agreements" -NoNewWindow -Wait
+Start-Process "winget.exe" -ArgumentList "install --id Zoom.Zoom --source winget --exact --accept-package-agreements --accept-source-agreements" -NoNewWindow -Wait
 
 Write-Host "⏳ Waiting for Zoom installation to complete..." -ForegroundColor Cyan
 
